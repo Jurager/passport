adbb1bc3afbc513a850248a98530f5b1b469f695
Yuri Gerassimov <jurager01@gmail.com>
2022-11-15 17:12:32 +0600

    fix

--- src/Exceptions/CustomProviderException.php
+++ src/Exceptions/CustomProviderException.php
@@ -8,6 +8,6 @@ class CustomProviderException extends Exception
 {
     public function __construct()
     {
-        parent::__construct('Choose a valid IP address lookup provider. The class must implement the ALajusticia\AuthTracker\Interfaces\IpProvider interface.');
+        parent::__construct('Choose a valid IP address lookup provider. The class must implement the Jurager\Passport\Interfaces\Provider interface.');
     }
 }

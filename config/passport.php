981ee8808acd2cd486112b6374bcb4e865e414e5
Yuri Gerassimov <jurager01@gmail.com>
2022-11-16 16:27:46 +0600

    fix config

--- config/passport.php
+++ config/passport.php
@@ -2,64 +2,151 @@
 
 return [
 
-    /**
-     * Configurations for SSO Server
-     */
+    /*
+    |--------------------------------------------------------------------------
+    | Configurations for SSO Server
+    |--------------------------------------------------------------------------
+    */
     'server' => [
 
-        /**
-        * Passport server driver, used to store brokers
-        * Supported drivers: "model", "array",
+        /*
+        |--------------------------------------------------------------------------
+        | Driver
+        |--------------------------------------------------------------------------
+        |
+        | Passport server driver, used to store brokers
+        | Supported values: "model", "array"
+        |
         */
+
         'driver' => env('PASSPORT_SERVER_DRIVER', 'model'),
 
-        /**
-         * Broker model class, required for "model" driver.
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | Model
+        |--------------------------------------------------------------------------
+        |
+        | Broker model class, required for "model" driver.
+        |
+        */
+
         'model' => env('PASSPORT_SERVER_MODEL', 'App\Models\Broker'),
 
-        /**
-         * Broker model id field, required for model driver.
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | Model Identification Field
+        |--------------------------------------------------------------------------
+        |
+        |  Broker model id field, required for model driver.
+        |
+        */
+
         'id_field' => env('PASSPORT_SERVER_ID_FIELD', 'field'),
 
-        /**
-         * Broker model secret field, required for model driver.
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | Model Secret Field
+        |--------------------------------------------------------------------------
+        |
+        |  Broker model secret field, required for model driver.
+        |
+        */
+
         'secret_field' => env('PASSPORT_SERVER_SECRET_FIELD', 'secret'),
 
-        /**
-         * Array of available brokers and it's secrets, required for "array" driver. ['id' => 'secret']
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | Brokers
+        |--------------------------------------------------------------------------
+        |
+        |  Array of available brokers and it's secrets, required for "array" driver. ['id' => 'secret']
+        |
+        */
+
         'brokers' => [],
 
-        /**
-         * Choose which parser to use to parse the User-Agent. Supported values: 'agent', 'whichbrowser'
-         */
-        'parser' => 'whichbrowser',
+        /*
+        |--------------------------------------------------------------------------
+        | Parser
+        |--------------------------------------------------------------------------
+        |
+        | Choose which parser to use to parse the User-Agent.
+        | You will need to install the package of the corresponding parser.
+        |
+        | Supported values:
+        | 'agent' (see https://github.com/jenssegers/agent)
+        | 'whichbrowser' (see https://github.com/WhichBrowser/Parser-PHP)
+        |
+        */
+
+        'parser' => 'agent',
+
+        /*
+        |--------------------------------------------------------------------------
+        | IP Address Lookup
+        |--------------------------------------------------------------------------
+        |
+        | This package provides a feature to get additional data about the client
+        | IP (like the geolocation) by calling an external API.
+        |
+        | This feature makes usage of the Guzzle PHP HTTP client to make the API
+        | calls, so you will have to install the corresponding package
+        | (see https://github.com/guzzle/guzzle) if you are considering to enable
+        | the IP address lookup.
+        |
+        */
 
         'lookup' => [
 
-            /**
-             * If you want to enable the IP address lookup, choose a supported lookup provider
-             * Supported values:
-             * - 'ip2location-lite' (see https://lite.ip2location.com/database/ip-country-region-city)
-             * - 'ip-api' (see https://members.ip-api.com)
-             * - false (to disable the IP address lookup feature)
-             * - any other custom name declared as a key of the custom_providers array
-             */
+            /*
+            |--------------------------------------------------------------------------
+            | Provider
+            |--------------------------------------------------------------------------
+            |
+            | If you want to enable the IP address lookup, choose a supported
+            | IP address lookup provider.
+            |
+            | Supported values:
+            | - 'ip2location-lite' (see https://lite.ip2location.com/database/ip-country-region-city)
+            | - 'ip-api' (see https://members.ip-api.com)
+            | - false (to disable the IP address lookup feature)
+            | - any other custom name declared as a key of the custom_providers array
+            |
+            */
+
             'provider' => 'ip-api',
 
 
-            /**
-             * Float describing the number of seconds to wait while trying to connect  to the provider's API.
-             */
+            /*
+            |--------------------------------------------------------------------------
+            | Timeout
+            |--------------------------------------------------------------------------
+            |
+            | Float describing the number of seconds to wait while trying to connect
+            | to the provider's API.
+            |
+            | If the request takes more time, the IP address lookup will be ignored
+            | and the Jurager\Passport\Events\FailedApiCall will be
+            | dispatched, receiving the attribute $exception containing the
+            | GuzzleHttp\Exception\TransferException.
+            |
+            | Use 0 to wait indefinitely.
+            |
+            */
+
             'timeout' => 1.0,
 
 
-            /**
-             * Indicate here an array of environments for which you want to enable address lookup.
-             */
+            /*
+            |--------------------------------------------------------------------------
+            | Environments
+            |--------------------------------------------------------------------------
+            |
+            | Indicate here an array of environnments for which you want to enable
+            | the IP address lookup.
+            |
+            */
+
             'environments' => [
                 'production',
             ],
@@ -96,125 +183,83 @@ return [
         ],
     ],
 
-    /**
-    * Configurations for SSO Client
+    /*
+    |--------------------------------------------------------------------------
+    | Configurations for SSO Client
+    |--------------------------------------------------------------------------
     */
+
     'broker' => [
 
-        /**
-        * Broker id for client configuration. Must be null on Server. Must match any word [a-zA-Z0-9_]
+        /*
+        |--------------------------------------------------------------------------
+        | Broker ID
+        |--------------------------------------------------------------------------
+        |
+        | Broker id for client configuration. Must be null on Server.
+        | Must match any word [a-zA-Z0-9_]
+        |
         */
 
         'client_id' => env('PASSPORT_BROKER_CLIENT_ID'),
 
-        /**
-        * Broker secret for client configuration. Must be null on SSO Server
+
+        /*
+        |--------------------------------------------------------------------------
+        | Broker Secret
+        |--------------------------------------------------------------------------
+        |
+        | Broker secret for client configuration. Must be null on Server.
+        |
         */
+
         'client_secret' => env('PASSPORT_BROKER_CLIENT_SECRET'),
 
-        /**
-        * Broker client unique username
+
+        /*
+        |--------------------------------------------------------------------------
+        | Broker Client Username
+        |--------------------------------------------------------------------------
+        |
+        | Broker client unique username field
+        |
         */
+
         'client_username' => env('PASSPORT_BROKER_CLIENT_USERNAME', 'email'),
 
-        /**
-        * The server Url. Required for clients.
+
+        /*
+        |--------------------------------------------------------------------------
+        | Server URL
+        |--------------------------------------------------------------------------
+        |
+        | The server Url. Required for clients.
+        |
         */
+
         'server_url' => env('PASSPORT_BROKER_SERVER_URL'),
 
-        /**
-         * The auth broker Url. Required if you want only on one broker
-         */
+        /*
+        |--------------------------------------------------------------------------
+        | Auth URL
+        |--------------------------------------------------------------------------
+        |
+        | The auth broker Url. Required if you want only on one broker
+        |
+        */
+
         'auth_url' => env('PASSPORT_BROKER_AUTH_URL'),
 
-        /**
-        * The return Url. Required for clients.
+        /*
+        |--------------------------------------------------------------------------
+        | Return URL
+        |--------------------------------------------------------------------------
+        |
+        | The return Url. Required for clients.
+        |
         */
+
         'return_url' => env('PASSPORT_BROKER_RETURN_URL', true),
-    ],
-
-    'lookup' => [
-
-        /*
-        |--------------------------------------------------------------------------
-        | Provider
-        |--------------------------------------------------------------------------
-        |
-        | If you want to enable the IP address lookup, choose a supported
-        | IP address lookup provider.
-        |
-        | Supported values:
-        | - 'ip2location-lite' (see https://lite.ip2location.com/database/ip-country-region-city)
-        | - 'ip-api' (see https://members.ip-api.com)
-        | - false (to disable the IP address lookup feature)
-        | - any other custom name declared as a key of the custom_providers array
-        |
-        */
-
-        'provider' => false,
-
-        /*
-        |--------------------------------------------------------------------------
-        | Timeout
-        |--------------------------------------------------------------------------
-        |
-        | Float describing the number of seconds to wait while trying to connect
-        | to the provider's API.
-        |
-        | If the request takes more time, the IP address lookup will be ignored
-        | and the Jurager\Passport\Events\FailedApiCall will be
-        | dispatched, receiving the attribute $exception containing the
-        | GuzzleHttp\Exception\TransferException.
-        |
-        | Use 0 to wait indefinitely.
-        |
-        */
-
-        'timeout' => 1.0,
-
-        /*
-        |--------------------------------------------------------------------------
-        | Environments
-        |--------------------------------------------------------------------------
-        |
-        | Indicate here an array of environments for which you want to enable
-        | the IP address lookup.
-        |
-        */
-
-        'environments' => [
-            'production',
-        ],
-
-        /*
-        |--------------------------------------------------------------------------
-        | Custom Providers
-        |--------------------------------------------------------------------------
-        |
-        | You can create your own custom providers for the IP address lookup feature.
-        | See in the README file how to create an IP provider class and declare it
-        | in the array below.
-        |
-        | Format: 'name_of_your_provider' => ProviderClassName::class
-        |
-        */
-
-        'custom_providers' => [],
-
-        /*
-        |--------------------------------------------------------------------------
-        | Ip2Location
-        |--------------------------------------------------------------------------
-        |
-        | If you are using 'ip2location-lite' provider, here you may change the
-        | name of the tables for IPv4 and IPv6.
-        |
-        */
-
-        'ip2location' => [
-            'ipv4_table' => 'ip2location_db3',
-            'ipv6_table' => 'ip2location_db3_ipv6',
-        ],
     ],
 
     /**

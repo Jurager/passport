4e8b2d450f07f2c79a0b51da5c9263364ea2f455
Yuri Gerassimov <jurager01@gmail.com>
2022-11-19 11:52:34 +0600

    fix

--- src/Providers/IpApi.php
+++ src/Providers/IpApi.php
@@ -18,7 +18,7 @@ class IpApi implements Provider
      */
     public function getRequest()
     {
-        return new GuzzleRequest('GET', 'https://ip-api.com/json/' . Request::ip() . '?fields=25');
+        return new GuzzleRequest('GET', 'http://ip-api.com/json/' . Request::ip() . '?fields=25');
     }
 
     /**

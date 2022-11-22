8400304db9a77a01f92c93f3efddded54c90687d
Yuri Gerassimov <jurager01@gmail.com>
2022-11-22 15:47:35 +0600

    fix

--- src/PassportServiceProvider.php
+++ src/PassportServiceProvider.php
@@ -82,7 +82,7 @@ class PassportServiceProvider extends ServiceProvider
 
                 // Run prunable command
                 //
-                $schedule->command('history:prune')->everyMinute();
+                //$schedule->command('history:prune')->everyMinute();
             });
         }
     }

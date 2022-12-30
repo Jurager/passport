55335490b9e5d24d1d3f358088c44d61568309e6
Yuri Gerassimov <jurager01@gmail.com>
2022-12-30 13:24:30 +0600

    fix

--- src/Http/Middleware/ClientAuthenticate.php
+++ src/Http/Middleware/ClientAuthenticate.php
@@ -6,7 +6,7 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
 
-class Authenticate extends Middleware
+class ClientAuthenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
